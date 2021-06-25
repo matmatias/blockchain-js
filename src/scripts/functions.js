035b1013a5898b32c93754e7084e8f3fe913e572
Matheus Matias <matheusomr1@gmail.com>
2021-06-25 17:25:40 -0300

    addBlock Button function done

--- src/scripts/functions.js
+++ src/scripts/functions.js
@@ -3,9 +3,13 @@ import Blockchain from '../blockchain.js';
 
 // Initialize Blockchain Button
 let initializeBlockchainButton = document.getElementById('initialize-blockchain-button');
-// Add Block button
+// Add Genesis Block button
 let addGenesisBlockButton = document.getElementById('add-genesis-block-button');
+// Add Block Button
+let addBlockButton = document.getElementById('add-block-button');
+
 let blockchain = null;
+let counter = 1;
 // Initializes an empty blockchain
 function intializeBlockchain() {
   blockchain = new Blockchain(3);
@@ -29,5 +33,18 @@ function addGenesisBlock() {
   console.log(blockchain);
 }
 
+function addBlock() {
+  // Creates a new block and adds it to the blockchain
+  counter++;
+  let newBlock = new Block(`Block: ${counter}`);
+  blockchain.addBlock(newBlock);
+  // Creates a new div with the blockchain-block class and adds it to the section "blockchain-section"
+  let newBlockDiv = document.createElement("div");
+  newBlockDiv.classList.add("blockchain-block");
+  newBlockDiv.innerHTML = blockchain.getLatestBlock().getBlockInfo();
+  document.getElementById("blockchain-section").appendChild(newBlockDiv);
+}
+
 initializeBlockchainButton.addEventListener('click', () => intializeBlockchain());
 addGenesisBlockButton.addEventListener('click', () => addGenesisBlock());
+addBlockButton.addEventListener('click', () => addBlock());
