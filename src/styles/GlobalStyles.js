2ba64ec15f13d5ff90720790470e82313640942b
Matheus Matias <matheusomr1@gmail.com>
2021-07-04 20:24:23 -0300

    Styled components installed

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: none;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+`
